2939b9df7b973e911349aebe6b10157c1247f71f
kevin <kevin@static>
2026-06-11 10:39:14 -0400

    .

---
+++ Colegio/src/main/java/com/mycompany/colegio/Colegio.java
@@ -0,0 +1,10 @@
+package com.mycompany.colegio;
+
+import com.mycompany.colegio.ui.Menu;
+
+
+public class Colegio {
+    public static void main(String[] args) {
+        new Menu().iniciar();
+    }
+}
